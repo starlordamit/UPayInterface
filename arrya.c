59348419a28cb6aceab52e3c68312106de8667b7
starlordamit <gabru.ahir@gmail.com>
2023-09-22 23:36:50 +0530

    76

---
+++ arrya.c
@@ -0,0 +1,18 @@
+//write a code in c to print a line
+
+#include<stdio.h>
+int main()
+{
+    //write a code in c to draw a line on screen pixel
+    int i,j;
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    
+}
